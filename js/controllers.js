c8f6698aa1143d469bd71a1148b1d0ef7cf688f4
Asim K T <asim@alraislabs.com>
2014-10-22 13:17:13 +0530

    send query string to server

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,16 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-	$scope.search = function(){
-		window.location = "#/tab/cart";
+.controller('DashCtrl', function($scope, $http) {
+	$scope.search = function(query){
+    $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+    $http.post('http://localhost/emarketserver/index.php', "name=" + query).
+      success(function(data, status, headers, config) {
+        alert("ok" + status + data);
+      }).
+      error(function(data, status, headers, config) {
+        alert("error" + status);
+      });
+      window.location = "#/tab/dash/" + query;
 	}
 })
 
@@ -36,4 +44,30 @@ angular.module('starter.controllers', [])
 })
 
 .controller('AccountCtrl', function($scope) {
-});
+})
+
+.controller('FetchController', ['$scope', '$http', '$templateCache',
+  function($scope, $http, $templateCache) {
+    $scope.method = 'GET';
+    $scope.url = 'http-hello.html';
+
+    $scope.fetch = function() {
+      $scope.code = null;
+      $scope.response = null;
+
+      $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+        success(function(data, status) {
+          $scope.status = status;
+          $scope.data = data;
+        }).
+        error(function(data, status) {
+          $scope.data = data || "Request failed";
+          $scope.status = status;
+      });
+    };
+
+    $scope.updateModel = function(method, url) {
+      $scope.method = method;
+      $scope.url = url;
+    };
+  }]);
